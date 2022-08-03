57d8b6977db07e264a839516f7c96435320cd0c3
Joel Adewole <joeladewole3@gmail.com>
2022-08-03 04:45:01 +0100

    Remove hook usage

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
-  // <OneidProvider apiKey="vxCgAQ==.fUF/4jTXK8FTGqqt31iDUIpMkYXr3y+Lwxy9pMtVW2Y=" siteDomain="http://localhost:3000">
-  // </OneidProvider>
 );
 
